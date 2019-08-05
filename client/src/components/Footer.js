0f30d92b592cc6157f42c9dbfb3b21af49d7b88a
Mike Carbone <michael.carbone@revzilla.com>
2019-08-05 17:04:56 -0400

    :lipstick: Footer adjustments

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
       </div>
     </div> */}
     <div className="footer__copy">
-      <div className="generic__standard-wrapper generic__standard-wrapper--vert">
+      <div className="footer__wrapper generic__standard-wrapper generic__standard-wrapper--vert">
         <p className="footer__copywrite">© {new Date().getFullYear()} Carbon Collective&trade;</p>
         <p className="footer__love">
           Built with <span className="generic__emoji">❤️</span> by <a className="footer__love-link generic__text-link" href="https://twitter.com/carbnology" target="_blank" rel="noopener noreferrer">Mike Carbone</a>
