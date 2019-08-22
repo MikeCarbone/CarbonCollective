a47c73d56e20adcd2a30e3e408e8d8c47d0a5383
Mike Carbone <michael.carbone@revzilla.com>
2019-08-22 11:45:05 -0400

    :bug: Fixes pagination margin jumping

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -106,11 +106,11 @@ class Homepage extends Component {
 
     const allPostsLoadedText = (this.state.allPostsLoaded)
       ? <p className="generic__p thats-all">That's all for now! <span aria-label="" role="img" className="generic__emoji">😎</span></p>
-      : <p></p>;
+      : <p className="generic__p generic__hidden thats-all">That's all for now! <span aria-label="" role="img" className="generic__emoji">😎</span></p>;
 
     const loadingAnimation = (this.state.isFetching)
       ? <img className="loading" src="images/pacman-load.svg" alt="Loading"></img>
-      : <img className="loading loading--hidden" src="images/pacman-load.svg" alt=""></img>
+      : <img className="loading generic__hidden" src="images/pacman-load.svg" alt=""></img>
 
     return (
       <main>
