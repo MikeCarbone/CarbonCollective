45bfce41f0a11b9489964968b4b584bf375d8375
Mike Carbone <michael.carbone@revzilla.com>
2019-09-12 11:28:43 -0400

    :lipstick: Improves styling of socialAsk

--- client/src/components/SocialAsk.js
+++ client/src/components/SocialAsk.js
@@ -3,10 +3,10 @@ import React from 'react';
 const SocialAsk = props => (
   <div className="social-ask">
     <h2 className="social-ask__header">THANK YOU</h2>
-    <p className="social-ask__text">Consider following our Twitter for updates when new things are posted!</p>
+    <div className="social-ask__hr cc__hr"></div>
+    <p className="social-ask__text">Consider following our Twitter when new updates are posted!</p>
     <a className="cc__btn social-ask__btn" href="https://twitter.com/crbncllctv" target="_blank" rel="noopener noreferrer">FOLLOW</a>
     <button className="social-ask__quiet" onClick={props.stopAsking}>I'm good my guy; stop showing me this</button>
-    <div className="cc__hr"></div>
   </div>
 );
 
